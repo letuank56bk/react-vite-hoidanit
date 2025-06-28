154b2cf2bf906d17ffcedb63ade1cf2ac5d1228b
Alan Le <letuank56bk@gmail.com>
2025-06-28 22:08:17 +0700

    Bai 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,14 @@
 import './style.css';
 
 const MyComponent = () => {
+    const hoidanit = "Alan check 1";
     return (
         <>
-            <div>Alan - CTTV</div>
-            <div className="child"> Child </div>
+            <div>{hoidanit} - CTTV</div>
+            <div>{console.log("Check function in HTML")}</div>
+            <div className="child"
+                style={{ color: 'blue', fontSize: '20px' }}
+            > Child </div>
         </>
     );
 }
