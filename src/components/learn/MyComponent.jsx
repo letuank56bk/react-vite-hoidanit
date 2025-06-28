6a5afab965c1381da5a12fc1ae47ad5ef3278bb7
Alan Le <letuank56bk@gmail.com>
2025-06-28 14:33:48 +0700

    Bai 29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,8 @@
+
+const MyComponent = () => {
+    return (
+        <div>Alan - CTTV</div>
+    );
+}
+
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,17 @@
+const SecondComponent = () => {
+    return (
+        <div>
+            <p>Second Component</p>
+        </div>
+    );
+}
+
+const ThirdComponent = () => {
+    return (
+        <div>
+            <p>Third Component</p>
+        </div>
+    );
+}
+
+export { SecondComponent, ThirdComponent };
