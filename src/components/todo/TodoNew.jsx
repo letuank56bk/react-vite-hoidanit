0746bc3e2a713f17b28a84aa5d1cd29b6e2da3c5
Alan Le <letuank56bk@gmail.com>
2025-07-01 15:48:00 +0700

    Bai 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,12 +1,23 @@
 const TodoNew = (props) => {
     console.log(">>> check props in TodoNew: ", props);
     const { addNewTodo } = props;
-    addNewTodo("Alan Le");
+    // addNewTodo("Alan Le");
+    const handleClick = () => {
+        alert("Click me!");
+    }
+
+    const handleOnChange = (name) => {
+        console.log(">>> check input value: ", name);
+    }
 
     return (
         <div className="todo-new">
-            <input type="input" />
-            <button>Add</button>
+            <input
+                onChange={(e) => { handleOnChange(e.target.value) }}
+                type="input" />
+            <button
+                onClick={handleClick}
+                style={{ cursor: "pointer" }}>Add</button>
         </div>
     )
 }
