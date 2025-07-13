2ea7893e19ab4402035417b4bebb6b3be9da26fd
Alan Le <letuank56bk@gmail.com>
2025-07-14 06:15:32 +0700

    Bai 70

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,7 +1,8 @@
-import { Input } from 'antd';
+import { Input, notification } from 'antd';
 import { Button } from 'antd';
 import { useState } from 'react';
-import axios from 'axios';
+import { createUserAPI } from '../../services/api.service';
+
 
 const UserForm = () => {
     const [fullName, setFullName] = useState('');
@@ -9,16 +10,19 @@ const UserForm = () => {
     const [password, setPassword] = useState('');
     const [phone, setPhone] = useState('');
 
-    const handleClickBtn = () => {
-        const URL_BACKEND = 'http://localhost:8080/api/v1/user';
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone,
+    const handleClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone);
+        // Data đã được xử lý thông qua interceptor
+        if (res.data) {
+            notification.success({
+                message: "Create user",
+                description: "Tạo user thành công",
+            })
         }
-        axios.post(URL_BACKEND, data)
+
+        console.log(">>> Check res", res.data);
     }
+
 
     return (
         <div className="user-form" style={{ margin: '20px 0' }}>
