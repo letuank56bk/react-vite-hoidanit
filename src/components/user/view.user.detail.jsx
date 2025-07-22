9d7c2d3465f0cc953212bd0952b438d074a4f6c0
Alan Le <letuank56bk@gmail.com>
2025-07-22 15:05:01 +0700

    Bai 82

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,10 +1,11 @@
-import { Drawer } from "antd";
+import { Button, Drawer } from "antd";
 
 const ViewUserDetail = (props) => {
     const { isDetailOpen, setIsDetailOpen, dataDetail, setDataDetail } = props;
 
     return (
         <Drawer
+            width={"40vw"}
             title="Detail User"
             closable={{ 'aria-label': 'Close Button' }}
             onClose={() => {
@@ -21,6 +22,26 @@ const ViewUserDetail = (props) => {
                 <p>Email: {dataDetail.email}</p>
                 <br />
                 <p>Phone: {dataDetail.phone}</p>
+                <br />
+                <p>Avatar:</p>
+                <div>
+                    <img height={"100"} width={"150"} src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
+                </div>
+                <div>
+                    <label
+                        htmlFor="btnUpload"
+                        style={{
+                            display: 'block',
+                            width: 'fit-content',
+                            marginTop: '15px',
+                            padding: '5px 10px',
+                            background: 'orange',
+                            borderRadius: '5px',
+                            cursor: 'pointer'
+                        }}>
+                        Upload Avatar</label>
+                    <input type="file" id="btnUpload" hidden />
+                </div>
             </> :
                 <><p>Không có dữ liệu...</p></>
             }
